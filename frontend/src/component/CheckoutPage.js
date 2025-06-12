7c1e55e2f929357dafed9a356144794f9cf508e6
godhaniaesha <aesha.kalathiyainfotech@gmail.com>
2025-06-12 12:10:39 +0530

    dfhgg

--- frontend/src/component/CheckoutPage.js
+++ frontend/src/component/CheckoutPage.js
@@ -231,11 +231,6 @@ const CheckoutPage = () => {
       for (const item of cartItems) {
         await dispatch(removeFromCart(item._id)).unwrap();
       }
-      
-      // Remove selected coupon from localStorage after successful order
-      localStorage.removeItem('selectedCoupon');
-      setSelectedCoupon(null);
-      
       setOrderId(orderResponse._id);
       setShowSuccessModal(true);
 

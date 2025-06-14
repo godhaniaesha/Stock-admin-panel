9134961cf14b090f673beb61bfb91568757e5255
architbhuva123 <kalathiyainfotecharchit@gmail.com>
2025-06-14 15:55:26 +0530

    sales report

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -4,17 +4,19 @@ import storage from 'redux-persist/lib/storage'
 import categoryReducer from './slice/category.slice'
 import subcategoryReducer from './slice/subCategory.slice'
 import productReducer from './slice/product.slice'
+import salesReducer from './slice/sales.slice'
 
 const persistConfig = {
     key: 'root',
     storage,
-    whitelist: ['category', 'subcategory', 'product']
+    whitelist: ['category', 'subcategory', 'product', 'sales']
 }
 
 const rootReducer = combineReducers({
     category: categoryReducer,
     subcategory: subcategoryReducer,
-    product: productReducer
+    product: productReducer,
+    sales: salesReducer
 })
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
